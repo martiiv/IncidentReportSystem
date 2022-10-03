bbef7b2ba9211f67fa59728140cb41eaf15ecc6e
aleksaabo1 <aleksanderaaboen@outlook.com>
2022-10-03 11:38:47 +0200

    added code and file hierarchy

---
+++ incidentAPI/communication/struct.go
@@ -0,0 +1,30 @@
+package communication
+
+type messageInput struct {
+	title       string
+	description string
+}
+
+type messageOutput struct {
+	Username  string `json:"username"`
+	AvatarURL string `json:"avatar_url"`
+	Embeds    `json:"embeds"`
+}
+
+type Embeds struct {
+	Author      `json:"author"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Color       int    `json:"color"`
+	Thumbnail   `json:"thumbnail"`
+}
+
+type Thumbnail struct {
+	URL string `json:"url"`
+}
+
+type Author struct {
+	Name    string `json:"name"`
+	URL     string `json:"url"`
+	IconURL string `json:"icon_url"`
+}
