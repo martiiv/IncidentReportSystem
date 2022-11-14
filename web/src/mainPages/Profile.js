0dfd83d2ce3a20b51b9118b9540d99a2af252fb2
aleksaabo1 <aleksanderaaboen@outlook.com>
2022-11-14 17:46:47 +0100

    Added all profile information

--- web/src/mainPages/Profile.js
+++ web/src/mainPages/Profile.js
@@ -4,13 +4,19 @@ import fetchData from "../middleware/FetchData";
 
 function Profile() {
 
-        const credentials = JSON.parse(sessionStorage.getItem("credentials"))
+    const credentials = JSON.parse(sessionStorage.getItem("credentials"))
+
         return (
             <div className="App">
             <span>
                 <p>Name: {credentials.userName}</p>
                 <p>Company: {credentials.company}</p>
-
+                <p>Email: {credentials.email}</p>
+                <p>ID: {credentials.cid}</p>
+                <button className={"btn"} onClick={() => {
+                    sessionStorage.clear()
+                    window.location.reload(true);}}
+                >Log out</button>
             </span>
             </div>
 
