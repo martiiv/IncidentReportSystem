b1411b200e87e99a483d0d180b47fe44ee1e3e57
aleksaabo1 <aleksanderaaboen@outlook.com>
2022-11-15 07:56:39 +0100

    Centered profile information

--- web/src/mainPages/Profile.js
+++ web/src/mainPages/Profile.js
@@ -1,26 +1,25 @@
-import {useEffect, useState} from "react";
-import {MANAGER_URL} from "../constants/WebURL";
-import fetchData from "../middleware/FetchData";
+import "./Profile.css"
 
 function Profile() {
 
     const credentials = JSON.parse(sessionStorage.getItem("credentials"))
 
-        return (
-            <div className="App">
-            <span>
+    return (
+        <div className="Profile">
+            <div className={"card"}>
                 <p>Name: {credentials.userName}</p>
                 <p>Company: {credentials.company}</p>
                 <p>Email: {credentials.email}</p>
                 <p>ID: {credentials.cid}</p>
-                <button className={"btn"} onClick={() => {
-                    sessionStorage.clear()
-                    window.location.reload(true);}}
-                >Log out</button>
-            </span>
             </div>
+            <button className={"btn"} onClick={() => {
+                sessionStorage.clear()
+                window.location.reload(true);
+            }}>Log out
+            </button>
+        </div>
 
-        );
+    );
 
 
 }
